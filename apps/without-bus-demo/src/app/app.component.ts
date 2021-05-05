33f0e099948fec63ccd7ecbe924ebc19b1753e40
Tom White <tomwhite007@gmail.com>
2021-05-05 07:27:08 +0100

    scaffold app

---
+++ apps/without-bus-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bus-demo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'without-bus-demo';
+}
