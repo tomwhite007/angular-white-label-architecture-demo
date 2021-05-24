fb2fb34ffd77dd7ac26877b537e5cfd0ab60976f
Tom White <tomwhite007@gmail.com>
2021-05-24 07:23:28 +0100

    feat: revert to callback lookup object in container component

--- apps/bus-demo/src/app/book-manager/book-manager-component-state.service.ts
+++ apps/bus-demo/src/app/book-manager/book-manager-component-state.service.ts
@@ -3,24 +3,7 @@ import { ComponentStore } from '@ngrx/component-store';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { BooksFacade } from '../+state/books.facade';
-import {
-  OutputEventObservableService,
-  outputEventSelector,
-} from '@gyrus/ui-io-bus';
-import { AddBookFormSubmitEvent } from './add-book-form/add-book-form.component';
-import {
-  BookListOutEvents,
-  BookListSelectBookEvent,
-} from './book-list/book-list.component';
-import { ShowFormCheckboxChangeEvent } from './show-form-checkbox/show-form-checkbox.component';
-import { TabsSelectTabEvent } from './tabs/tabs.component';
-import { OutputEventNames } from '../_shared/interfaces/bus-event-names.interface';
-
-export type OutputEvents =
-  | AddBookFormSubmitEvent
-  | BookListOutEvents
-  | ShowFormCheckboxChangeEvent
-  | TabsSelectTabEvent;
+import { BooksEntity } from '../+state/books.models';
 
 interface LocalState {
   showForm: boolean;
@@ -50,78 +33,26 @@ export class BookManagerComponentStateService extends ComponentStore<LocalState>
 
   readonly selectedTab$ = this.select((state) => state.selectedTab);
 
-  constructor(
-    private books: BooksFacade,
-    private outputService: OutputEventObservableService<OutputEvents>
-  ) {
+  constructor(private books: BooksFacade) {
     super({
       showForm: false,
       selectedTab: 0,
     });
   }
 
-  // proxy outputEventToObservable
-  outputEventToObservable(event: OutputEvents) {
-    this.outputService.outputEventToObservable(event);
-  }
-
   // Updaters
 
-  readonly toggleShowForm = this.updater((state, event: unknown) => ({
+  readonly toggleShowForm = this.updater((state) => ({
     ...state,
     showForm: !state.showForm,
-  }))(
-    outputEventSelector(
-      this.outputService.outBus$,
-      OutputEventNames.ShowFormCheckboxChange
-    )
-  );
+  }));
 
-  readonly setSelectedTab = this.updater(
-    (state, event: TabsSelectTabEvent) => ({
-      ...state,
-      selectedTab: event.payload,
-    })
-  )(
-    outputEventSelector(
-      this.outputService.outBus$,
-      OutputEventNames.TabsSelectTab
-    )
-  );
+  readonly setSelectedTab = this.updater((state, tabNo: number) => ({
+    ...state,
+    selectedTab: tabNo,
+  }));
 
   // Effects
-
-  readonly upsertBook = this.effect(
-    (event$: Observable<AddBookFormSubmitEvent>) => {
-      return event$.pipe(
-        tap((event: AddBookFormSubmitEvent) => {
-          this.books.upsertBook(event.payload);
-        })
-      );
-    }
-  )(
-    outputEventSelector(
-      this.outputService.outBus$,
-      OutputEventNames.AddBookFormSubmit
-    )
-  );
-
-  readonly selectBook = this.effect(
-    (event$: Observable<BookListSelectBookEvent>) => {
-      return event$.pipe(
-        tap((event: BookListSelectBookEvent) => {
-          this.books.selectBook(event.payload);
-        })
-      );
-    }
-  )(
-    outputEventSelector(
-      this.outputService.outBus$,
-      OutputEventNames.BookListSelectBook,
-      // BookListClearSelectedBook can use this effect because it sends a null payload
-      OutputEventNames.BookListClearSelectedBook
-    )
-  );
 
   readonly updateGoogleAnalyticsWithTabSelected = this.effect(
     (selectedTab$: Observable<number>) => {
@@ -133,14 +64,24 @@ export class BookManagerComponentStateService extends ComponentStore<LocalState>
             event: 'tabSelected',
             value: tab,
           });
+
+          console.log('window.dataLayer', window.dataLayer);
         })
       );
     }
   )(this.selectedTab$);
 
-  // Update Global state
+  // Global state
 
   loadBooks() {
     this.books.loadBooks();
+  }
+
+  upsertBook(book: BooksEntity) {
+    this.books.upsertBook(book);
+  }
+
+  selectBook(id: string) {
+    this.books.selectBook(id);
   }
 }
