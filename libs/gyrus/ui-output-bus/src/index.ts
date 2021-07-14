340f42038d7f47bb4e1c4d262c0d7b0bf6048c74
Tom White <tomwhite007@gmail.com>
2021-07-14 08:58:53 +0100

    remove output bus as stream tools

--- libs/gyrus/ui-output-bus/src/index.ts
+++ libs/gyrus/ui-output-bus/src/index.ts
@@ -1,7 +1,4 @@
 export * from './lib/services/ui-output-bus-logger.service';
-export * from './lib/services/output-event-observable.service';
 export * from './lib/interfaces/output-bus-event.interface';
 export * from './lib/helper-functions/out-bus-emit';
 export * from './lib/helper-functions/output-event-handler';
-export * from './lib/helper-functions/output-event-selector';
-export * from './lib/operators/filter-output-events.operator';
